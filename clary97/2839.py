b8b595ec0c9cc60b6d8fef171036e0ef665bf2c0
clary97 <clary9752@gmail.com>
2022-01-08 16:52:13 +0900

    백준 2839번 코드에 주석 추가

--- clary97/2839.py
+++ clary97/2839.py
@@ -1,9 +1,11 @@
 sugar = int(input())
 
-answer = -1
-for i in range(5):# 0 ~ 4
-    if (sugar - i*3)>=0 and (sugar - i*3)%5==0:
-        answer = i + (sugar - i*3)//5
+answer = -1 # 초기 답안은 -1 (불가능)
+
+for i in range(5):# 3kg 을 몇 번 사용할 것인지(i)
+    sugar_remain = sugar - 3*i #  3kg를 처리하고 남은 양(5kg만 써서 처리해야 함)
+    if sugar_remain>=0 and sugar_remain%5==0: # 남은 양이 5kg만 써서 처리 가능한가?
+        answer = i + sugar_remain//5 # 답 = (3kg 사용횟수) + (5kg 사용횟수)
         break
     
 print(answer)
