cd599c94115bc2f7e473a2ae0e2a482e7d78242b
clary97 <clary9752@gmail.com>
2022-01-08 17:25:21 +0900

    변수명 수정

--- clary97/10162.py
+++ clary97/10162.py
@@ -1,12 +1,12 @@
-money = int(input())
+button = int(input())
 
-money_types = [300, 60, 10]  # 5분은 300초, 1분은 60초
+button_types = [300, 60, 10]  # 5분은 300초, 1분은 60초
 counts = []
-for mon in money_types :
-    counts.append(money // mon)
-    money %= mon
+for mon in button_types :
+    counts.append(button // mon)
+    button%= mon
 
-if money == 0:
+if button == 0:
     print(f"{counts[0]} {counts[1]} {counts[2]}")
 else:
     print(-1)
