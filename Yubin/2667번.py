b0aa6e82013511eeff711b90ff4aeb918c75fe62
YuBin Kim <43837921+luwbe1@users.noreply.github.com>
2022-01-19 00:32:28 +0900

    Update 2667번.py

--- Yubin/2667번.py
+++ Yubin/2667번.py
@@ -15,7 +15,7 @@ def bfs(graph,x,y):
     queue = deque()
     queue.append((x,y))
     graph[x][y] = 0
-    count = 1
+    count = 1 # 0으로 바꿔주니깐 count 1부터 시작
     while queue:
         x, y = queue.popleft()
         graph[x][y] = 0 # 0으로 바꿔주기
@@ -23,21 +23,21 @@ def bfs(graph,x,y):
             Nx = x + dx[i]
             Ny = y + dy[i]
 
-            if Nx < 0 or Ny < 0 or Nx >= N or Ny >= N:
+            if Nx < 0 or Ny < 0 or Nx >= N or Ny >= N: # 벗어나면 다음
                 continue
-            if graph[Nx][Ny] == 1:
+            if graph[Nx][Ny] == 1: 
                 graph[Nx][Ny] = 0
                 queue.append((Nx, Ny))
                 count += 1
-    return count
+    return count # count를 리턴함
 
 for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 1:
-            count = bfs(graph, i, j)
-            result.append(count)
+    for j in range(N): # NxN이니깐 
+        if graph[i][j] == 1: # 1이면 
+            count = bfs(graph, i, j) 
+            result.append(count) # result에 차례대로 
 
-result.sort()
+result.sort() # 정렬해서 보여주기 오름차순으로
 
 print(len(result)) 
 for k in result:
