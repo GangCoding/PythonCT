d96bbd08bafcd1b60d3571538758e983f8915e65
clary97 <clary9752@gmail.com>
2022-01-19 20:29:23 +0900

    입력 수정

--- clary97/6-12.py
+++ clary97/6-12.py
@@ -2,9 +2,8 @@ from audioop import reverse
 
 n, k = map(int, input().split())
 
-for i in range(n) :
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
     
 a.sort()
 b.sort(reverse=True)
