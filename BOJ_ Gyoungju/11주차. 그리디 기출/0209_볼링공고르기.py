739494407b9253541da588cd97c5c8062e05b305
qw-4735 <78010864+qw-4735@users.noreply.github.com>
2022-02-13 19:11:43 +0900

    Update 0209_볼링공고르기.py

--- BOJ_ Gyoungju/11주차. 그리디 기출/0209_볼링공고르기.py
+++ BOJ_ Gyoungju/11주차. 그리디 기출/0209_볼링공고르기.py
@@ -6,13 +6,12 @@ Created on Wed Feb  9 19:01:48 2022
 """
 
 n,m = map(int, input().split())  # n=5 , m=3
-gongs=[]  # 1 3 2 3 2  # 고으이 무게 1 5 4 3 2 4 5 2
+gongs=[]  # 1 3 2 3 2  # 공의 무게 1 5 4 3 2 4 5 2
 cnt =0  # 두 사람이 공을 고르는 경우의 수 25 = 7+ 10 +4 +4  
-#/8=1(무게1의개수)*4 +2(무게2의 개수)*2 
+
 for i in range(n):
     gongs.append(int(input()))        
 for i in range(1,m):  # 1 > 2
     n -= gongs.count(i)
     cnt += gongs.count(i)*n  
-    # cnt = 1*(5-1-gongs.count(0)) 2* (5-2-1)
 print(cnt)    
