cb728185d33d306d1c6c32e64d547754b58289e8
YuBin Kim <43837921+luwbe1@users.noreply.github.com>
2022-02-13 20:52:27 +0900

    Update 08.py

--- Yubin/part03/08.py
+++ Yubin/part03/08.py
@@ -1,12 +1,7 @@
-import re
-
 S = input()
 
-# a = []
-
-# for i in S:
-#     a.append(i)
 sum = 0
+
 numbers = int(''.join(list(filter(str.isdigit, S))))
 numbers = list(map(int, str(numbers)))
 string = sorted(list(filter(str.isalpha, S)))
